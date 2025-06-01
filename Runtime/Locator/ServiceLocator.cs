6256fded5f4d8a0cce7f8ae3cbba2de3a0e193a0
Agustin Rosso <AgustinRosso_1@hotmail.com>
2025-06-01 16:38:39 +0200

    Se agrega tryGet al ServiceLocator

--- Runtime/Locator/ServiceLocator.cs
+++ Runtime/Locator/ServiceLocator.cs
@@ -12,6 +12,7 @@ namespace RossoForge.Services.Locator
         }
 
         public static void Initialize() => _current.Initialize();
+        public static bool TryGet<T>(out T service) where T : IService => _current.TryGet<T>(out service);
         public static T Get<T>() where T : IService => _current.Get<T>();
         public static void Register<T>(T service) where T : IService => _current.Register<T>(service);
         public static void Unregister<T>() where T : IService => _current.Unregister<T>();
