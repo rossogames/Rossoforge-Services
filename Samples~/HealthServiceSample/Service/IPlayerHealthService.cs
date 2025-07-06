45e928d4c2a4a450977bcdf18711d6b0056276c8
Agustin Rosso <AgustinRosso_1@hotmail.com>
2025-07-06 23:16:56 +0200

    actualizacion de sample

--- Samples~/HealthServiceSample/Service/IPlayerHealthService.cs
+++ Samples~/HealthServiceSample/Service/IPlayerHealthService.cs
@@ -1,6 +1,6 @@
-using RossoForge.Core.Services;
+using Rossoforge.Core.Services;
 
-namespace RossoForge.Services.Samples.PlayerHealth
+namespace Rossoforge.Services.Samples.PlayerHealth
 {
     public interface IPlayerHealthService : IService, IInitializable
     {
